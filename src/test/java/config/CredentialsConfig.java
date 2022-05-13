36b577d77e8557d89de4a3debdea84bf9f8d16f4
TatianaCheb <tanya.cheba12@gmail.com>
2022-05-13 13:44:16 +0300

    added project BrowserStack TestMobile

---
+++ src/test/java/config/CredentialsConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:credentials.properties"})
+public interface CredentialsConfig extends Config {
+    String user();
+    String key();
+    String app();
+    String url();
+}
---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,44 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import config.Credentials;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities caps) {
+
+        // Set your access credentials
+        caps.setCapability("browserstack.user", Credentials.config.user());
+        caps.setCapability("browserstack.key", Credentials.config.key());
+
+        // Set URL of the application under test
+        caps.setCapability("app", Credentials.config.app());
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Google Pixel 3");
+        caps.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "Second Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "test_new_app_wiki");
+
+
+        return new AndroidDriver(getBrowserstackUrl(), caps);
+    }
+}
